d84fba5788cbb09f326d4b8167e16c60b9d1967b
ViniAltF4 <santos.silva.marcos3112@escola.pr.gov.br>
2026-09-02 16:19:35 -0300

    2212

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-const   numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha=12;
+const numeroSenha = document.querySelector('.parametro-senha__texto');//selaciona o
+let tamanhoSenha=12;//variael que pode ser alterada
 
-numeroSenha.textContent=tamanhoSenha;
-const botoes=document.querySelectorAll('parametro-senha__botao');
+numeroSenha.textContent=tamanhoSenha;//atribui o tamnho Senha ao numeroSenha
 
-botoes[0].oneclick= diminuiTamanho;
+const botoes=document.querySelectorAll('.parametro-senha__botao');//selecionando todas as classes "parametro-senha__botao"
+
+const campoSenha=document.querySelector('#campo-senha');
+const letrasMaiusculas='ABCDEFG';
+
+botoes[0].onclick = diminuiTamanho;//Tirar os parenteses
+botoes[1].onclick = aumentaTamanho;
+
 function diminuiTamanho() {
-
-    if (tamanhoSenha>1) {
-            tamanhoSenha=tamanhoSenha-1;
-    }
-
-
-    numeroSenha.textContent=tamanhoSenha;
+if(tamanhoSenha >1){
+//tamanhoSenha=tamanhoSenha-1; //diminui de 1 o valor do tamanhoSenha
+tamanhoSenha--;
+}
+numeroSenha.textContent=tamanhoSenha;//atribui o tamanho Senha ao numeroSenha
+geraSenha();
 }
 
-botoes[1].oneclick= aumentaTamanho;
 function aumentaTamanho() {
+if(tamanhoSenha<20){
+tamanhoSenha = tamanhoSenha+1; //aumenta de 1 o valor do tamanhoSenha
+//tamanhoSenha++;
+}
+numeroSenha.textContent = tamanhoSenha;//atribui o tamanho Senha ao numeroSenha
+geraSenha();
+}
 
-    if (tamanhoSenha<20) {
-            tamanhoSenha=tamanhoSenha+1;
-    }
 
-
-    tamanhoSenha=tamanhoSenha+1;
-    numeroSenha.textContent=tamanhoSenha;
-
+function geraSenha(){
+let senha = '';
+for (let i = 0; i < tamanhoSenha;i++){
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+senha = senha + letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value = senha;
 }
